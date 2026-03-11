1f21b0aff3d73631811afa5a60939fe8e10e667f
Carla Marquez <carlaisabel21@hotmail.es>
2026-03-11 18:04:14 -0500

    Learning to update changes in the branch

--- Login/src/obj/Debug/net8.0/Login.AssemblyInfo.cs
+++ Login/src/obj/Debug/net8.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b8d0fe21357053590a8e61bbd171836300b87e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d558ecaf75f262269e7dade9ba41e3a0f0cc52df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
